e7e510756bde3646de8a64d84d783e2222d009e8
Fred <frederico.a.soares@gmail.com>
2019-06-26 18:03:06 -0300

    Added cors and helmet dependencies on project.

--- app/app.js
+++ app/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
+const cors = require('cors');
+const helmet = require('helmet');
+
 const toolsRouter = require('./routes/toolsRouter');
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
