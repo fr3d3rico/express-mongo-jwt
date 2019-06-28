cc3d22003fdb56ea36ec1bba985cf73091d02403
Fred <frederico.a.soares@gmail.com>
2019-06-28 18:15:04 -0300

    Added cookie-parser dependency.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 const helmet = require('helmet');
@@ -12,6 +14,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
