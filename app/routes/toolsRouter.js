7ed89cf960810d4e2b85b157b0eb6063a08affdc
Fred <frederico.a.soares@gmail.com>
2019-07-02 19:26:35 -0300

    Fixed #16 bug. Improvements and bug fixes.

--- app/routes/toolsRouter.js
+++ app/routes/toolsRouter.js
@@ -7,14 +7,21 @@ router.get('/tools', verifyJWT, (req, res) => {
     var query = {};
     new Promise(resolve => {
         if( req.query.tag ) {
-            query = {tag: req.query.tag};
+            query = {tags: req.query.tag};
             resolve();
         }
         resolve();
     });
     
     ToolSchema.find(query, (err, docs) => {
-        res.status(200).send(docs);
+        if(err) return res.status(500).send('Server error!(find): ' + err);
+
+        if(docs) {
+            res.status(200).send(docs);
+        }
+        else {
+            res.status(404).send('Items not found with related tag');
+        }
     });
 });
 
@@ -31,7 +38,7 @@ router.delete('/tools/:id', verifyJWT, (req, res) => {
     if( !req.params.id ) res.status(404).send('Parameter not found!').end();
 
     ToolSchema.deleteOne({_id: req.params.id}, (err, result) => {
-        if(err) return res.status(500).send('Server error!(save): ' + err).end();;
+        if(err) return res.status(500).send('Server error!(delete): ' + err).end();;
 
         if(result.deletedCount === 0) {
             res.status(404).send(result).end();
